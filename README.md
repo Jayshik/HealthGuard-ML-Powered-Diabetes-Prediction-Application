433aca3eb2271b5e49cd889abbf1884688b9cfd5
Jayesh Kaushik <124242618+Jayshik@users.noreply.github.com>
2024-02-19 11:13:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# HealthGuard-ML-Powered-Diabetes-Prediction-Application
+# dsa_ninja_turtles
+Use Case:
+      In this project we focuses on developing a machine learning model to predict the likelihood of women above age of 21 having diabetes. The dataset used for this project is sourced from Kaggle, which contains various features related to health indicators and diagnostic measurements. The goal is to create a simple and accurate prediction model for diabetes without spending extensive time on the model development step.The ML-Powered Application will enable users to assess their risk of having diabetes by providing their health indicators and receiving predictions instantly
+Dataset:
+source: kaggle (https://www.kaggle.com/datasets/nancyalaswad90/review)
+Architecture: 1.User Interface: A web application developed using Streamlit, providing prediction and past prediction functionalities.  
+              2.Model Service (API): An API developed using FastAPI to serve the machine learning model, make predictions, and store predictions in the database.
+              3.ML model: Using decision tree for prediction 
+              3.Database: An SQL database (PostgreSQL) used to store past predictions, along with the features used for prediction.
+              4.Prediction Job: A scheduled job implemented using Airflow that periodically makes predictions on the ingested data.
+              5.Great expection:  Data validation (blood pressure, glucose, age)
+              6.Monitoring Dashboard: A Grafana dashboard to monitor data quality problems and model prediction issues.
+                                      Created two dashboard i) XY- plot for Age vs Peregnincies
+                                                            	        ii) Bar guage
+               7.Workflow: User-end :      i)Users can access the web application to make on-demand predictions by filling in the features for a single sample or                                                     uploading a CSV file for multiple predictions.
+                                           ii)The web application calls the model service API to retrieve predictions, which are then displayed to the user along with                                                the corresponding features used for the prediction.
+    
+                          Prediction-end:   i)New data files are ingested and validated for quality using tools like Great Expectations.
+                                            ii)Clean data files are stored, while files with data quality issues are flagged and saved in the database.
+                                            iii)The prediction job, running every 5 minutes, checks for new clean data files.
+                                            iv)The prediction job reads the clean data files and makes API calls to the model service for predictions.
+                                            v)Predictions are stored in the database, enabling access to the latest predictions and historical prediction data.
+Installation:
+            1.Install the dependencies: pip install -r requirements.txt
+            2.Application setup: streamlit run app.py
+            3.Airflow: pip install apache-airflow
+             Initialize the Airflow Database: airflow db init
+             Start the Airflow Web Server and Scheduler: airflow webserver --port 8080
+                                                         airflow scheduler
+             4.Great Expectations: pip install great_expectations
+             5.Visualization tool Grafana: Downloaded from offical website  
+                                            echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+                                            sudo apt-get install -y adduser libfontconfig1
+                                            wget https://dl.grafana.com/enterprise/release/grafana-enterprise_10.0.0_amd64.deb
+                                            sudo dpkg -i grafana-enterprise_10.0.0_amd64.deb
+                                            sudo yum install -y https://dl.grafana.com/enterprise/release/grafana-enterprise-10.0.0-1.x86_64.rpm
+                                            wget https://dl.grafana.com/enterprise/release/grafana-enterprise-10.0.0-1.x86_64.rpm
+                                            sudo rpm -Uvh grafana-enterprise-10.0.0-1.x86_64.rpm
+ Usage:
+      Prediction Webpage: Use this (streamlit - Diabetic predictor)webpage to make predictions.
+      i)Fill in the features for a single sample prediction using the provided form.
+      ii)In order to make multiple predictions,need to upload a CSV file containing the inference data (without labels).
+      and then Clicking the "Predict" button to make on-demand predictions, which will be displayed in the webapp along with the used features.
+      
+      Past Predictions Webpage: Use this webpage to view past predictions.   
+      i) Select the start and end dates for the predictions.
+      ii) Choose the prediction source from the drop-down list (webapp, scheduled predictions, or all).
+         View the past predictions along with the corresponding used features.
+       Visualzation: Using grafana Data Quality Monitoring, Model Performance Monitoring , Data Drift Detection , Dashboard Customization and Alerting and                                  Notifications
+         
+ API Endpoints:
+The model service (API) exposes the following endpoints:
+    i)POST /predict: Make model predictions by providing the necessary input features.
+    ii)GET /past-predictions: Retrieve past predictions along with the used features.
+
+
+             
+      
+      
+      
+     
+
+
+
---
+++ fastAPI_DB.py
@@ -0,0 +1,161 @@
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from datetime import date, datetime
+import psycopg2
+import joblib
+import pandas as pd
+
+# Create a connection to the PostgreSQL database
+conn = psycopg2.connect(
+    host="localhost",
+    database="diabetes",
+    user="postgres",
+    password="root"
+)
+cursor = conn.cursor()
+
+# Create a FastAPI instance
+app = FastAPI()
+
+# Create a Pydantic model to represent the input data for the prediction
+
+class DiabetesData(BaseModel):
+    Pregnancies: int
+    Glucose: int
+    BloodPressure: int
+    SkinThickness: int
+    Insulin: int
+    BMI: float
+    DiabetesPedigreeFunction: float
+    Age: int
+
+# Define a function to insert a new prediction record into the database
+def insert_prediction(prediction_date, pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, pedigree_function, age, prediction_result, prediction_source):
+    sql = """
+        INSERT INTO predictions (prediction_date, pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, pedigree_function, age, prediction_result, prediction_source)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    cursor.execute(sql, (prediction_date, pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, pedigree_function, age, prediction_result, prediction_source))
+    conn.commit()
+
+# Placeholder function for making predictions
+def make_predictions(my_features: DiabetesData):
+
+    print("in predictions")
+    scaler = joblib.load("scaler.joblib")
+    model = joblib.load("model.joblib")
+
+        # Convert the input features to a DataFrame
+    df = pd.DataFrame([my_features.dict()])
+
+        # Perform data scaling with feature names
+    feature_names = df.columns
+    data_scaled = scaler.transform(df)
+    df_scaled = pd.DataFrame(data_scaled, columns=feature_names)
+
+        # Make predictions
+    prediction_result = model.predict(df_scaled)
+
+    out = int(prediction_result[0])
+    print(out,
+          'OUTPUT')
+
+    return out
+
+    #return 42
+
+# Define a FastAPI endpoint to load data from a CSV file and make predictions
+@app.post("/predict-from-csv")
+async def predict_from_csv(data: DiabetesData):
+    try:
+        # Decode the file content
+        print("in API")
+        prediction_result = make_predictions(data)
+
+
+        prediction_date = datetime.now()
+        pregnancies = data.Pregnancies
+        glucose = data.Glucose
+        blood_pressure = data.BloodPressure
+        skin_thickness = data.SkinThickness
+        insulin = data.Insulin
+        bmi = data.BMI
+        pedigree_function = data.DiabetesPedigreeFunction
+        age = data.Age
+        prediction_source = 'prediction job'
+
+        insert_prediction(prediction_date, pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, pedigree_function, age, prediction_result, prediction_source)
+
+        return {"predictions": prediction_result}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Define a FastAPI endpoint to handle the prediction request
+@app.post("/predict")
+async def predict(data: DiabetesData):
+    # Make prediction using input data
+    # Store prediction result and used features in the database
+    try:
+        # Make prediction using input data
+        # In this example, the prediction result is just the sum of all input features
+
+        prediction_result = make_predictions(data)
+        print(prediction_result)
+
+        # Store prediction result and used features in the database
+        prediction_date = datetime.now()
+        pregnancies = data.Pregnancies
+        glucose = data.Glucose
+        blood_pressure = data.BloodPressure
+        skin_thickness = data.SkinThickness
+        insulin = data.Insulin
+        bmi = data.BMI
+        pedigree_function = data.DiabetesPedigreeFunction
+        age = data.Age
+        prediction_source = 'webapp'
+
+        insert_prediction(prediction_date, pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, pedigree_function, age, prediction_result, prediction_source)
+
+        return {"prediction": prediction_result}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Define a function to retrieve past predictions from the database
+def get_past_predictions(start_date, end_date, prediction_source):
+    sql = """
+        SELECT prediction_date, pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, pedigree_function, age, prediction_result
+        FROM predictions
+        WHERE prediction_date BETWEEN %s AND %s
+        AND prediction_source = %s
+    """
+    cursor.execute(sql, (start_date, end_date, prediction_source))
+    rows = cursor.fetchall()
+    past_predictions =[
+        {
+            "prediction_date": row[0].strftime("%Y-%m-%d %H:%M:%S"),
+            "pregnancies": row[1],
+            "glucose": row[2],
+            "blood_pressure": row[3],
+            "skin_thickness": row[4],
+            "insulin": row[5],
+            "bmi": row[6],
+            "pedigree_function": row[7],
+            "age": row[8],
+            "prediction": row[9]
+            } for row in rows
+    ]
+    return past_predictions
+
+
+# Define a FastAPI endpoint to retrieve past predictions from the database
+@app.get("/past-predictions")
+async def past_predictions(start_date: date, end_date: date, prediction_source: str = 'all'):
+    # Retrieve past predictions from the database and return them as a JSON response
+    try:
+        past_predictions = get_past_predictions(start_date, end_date, prediction_source)
+        return past_predictions
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
